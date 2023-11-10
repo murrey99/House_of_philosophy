6732be168040378629afe084cf2d2020c652a6ee
murrey99 <135708368+murrey99@users.noreply.github.com>
2023-11-10 10:13:33 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="text-4xl text-semibold text-center ">
+   <h1>House_of_philosophy</h1>
+    </main>
+  )
+}
